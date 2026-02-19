2a05c8ff27f01d56e35767ebe02961452b3cbd08
dnlsvdr <danielorcasaavedra778@gmail.com>
2026-02-19 14:48:57 -0800

    fix routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,47 +1,31 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../pages/HomePage";
+import { createHashRouter } from "react-router-dom";
 import App from "../App";
+import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 import StudentListPage from "../pages/StudentListPage";
 import StudentDetailPage from "../pages/StudentDetailPage";
 import NotFoundPage from "../components/NotFoundPage";
 
-/**
- * The router configuration for the application.
- */
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "/",
-                element: (
-                    <HomePage
-                        title={"Hello, welcome to students-react with TypeScript!"}
-                        subtitle={"You can view some of your favorite students here."}
-                    >
-                        Surely, school is fun.
-                    </HomePage>
-                ),
-            },
-            {
-                path: "/about",
-                element: <AboutPage message={"Application that uses student data."} />,
-            },
-            {
-                path: "/list",
-                element: <StudentListPage />,
-            },
-            {
-                path: "/detail/:id",
-                element: <StudentDetailPage />,
-            },
-            {
-                path: "*",
-                element: <NotFoundPage />,
-            },
-
-        ],
-    },
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: (
+          <HomePage
+            title={"Hello, welcome to students-react with TypeScript!"}
+            subtitle={"You can view some of your favorite students here."}
+          >
+            Surely, school is fun.
+          </HomePage>
+        ),
+      },
+      { path: "about", element: <AboutPage message={"Application that uses student data."} /> },
+      { path: "list", element: <StudentListPage /> },
+      { path: "detail/:id", element: <StudentDetailPage /> },
+      { path: "*", element: <NotFoundPage /> },
+    ],
+  },
 ]);
